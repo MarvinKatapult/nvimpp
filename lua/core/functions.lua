9e341f3d12a65683ae056f6dfb0ec90e3905ff9f
MarvinKatapult <ibingmarvin@gmail.com>
2025-04-04 23:13:19 +0200

    Made Cmd Window bigger

--- lua/core/functions.lua
+++ lua/core/functions.lua
@@ -65,10 +65,10 @@ function M.ExecuteSavedCmd()
     local buf = vim.api.nvim_create_buf(false, true)
     local win = vim.api.nvim_open_win(buf, true, {
         relative = 'editor',
-        width = math.floor(vim.o.columns * 0.8),
-        height = math.floor(vim.o.lines * 0.8),
-        row = math.floor(vim.o.lines * 0.1),
-        col = math.floor(vim.o.columns * 0.1),
+        width = math.floor(vim.o.columns * 0.9),
+        height = math.floor(vim.o.lines * 0.9),
+        row = math.floor(vim.o.lines * 0.05),
+        col = math.floor(vim.o.columns * 0.05),
         style = 'minimal',
         border = 'double',
     })
